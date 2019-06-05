2dc170798044afeca80026958afa5ea35c243b3d
bobbylemm <chidozienwoga@gmail.com>
2019-06-05 09:45:50 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+// these are all the configurations I need to strart using Rx with the Vue application. Additional functionality would then provided to the my other components.
+import Rx from 'rxjs/Rx';
+import VueRx from 'vue-rx';
+
+import axios from 'axios';
+import vueAxios from 'vue-axios';
+
+Vue.use(VueRx, Rx);
+Vue.use(vueAxios, axios);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App)
+}).$mount('#app');
